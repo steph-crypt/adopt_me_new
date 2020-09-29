aefc3518df3e11f325e952108831df732e53887d
Sofia <sgandsteph@gmail.com>
2020-09-29 11:51:38 +0200

    fixes bug in react import

---
+++ src/Pet.js
@@ -0,0 +1,7 @@
+const Pet = ({ name, animal, breed }) => {
+return React.createElement("div", {}, [
+  React.createElement("h1", {}, name),
+  React.createElement("h2", {}, animal),
+  React.createElement("h2", {}, breed)
+  ]);
+};
