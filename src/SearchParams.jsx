49d1a69a64db86e7b7f21f65f667877373fe1991
Sofia <sgandsteph@gmail.com>
2020-09-30 23:26:26 +0200

    Adds animal hook/option to form

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seatle, WA");
-
+  const [animal, setAnimal] = useState("Cat");
   return (
     <div className="search-params">
       <h1>{location}</h1>
@@ -13,8 +14,21 @@ const SearchParams = () => {
             id="location"
             value={location}
             placeholder="Location"
-            onChange={e => setLocation(e.target.value)}
-          />
+            onChange={e => setLocation(e.target.value)} />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <input
+            id="animal"
+            value={animal}
+            placeholder="Location"
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)} />
+
+          <option>All</option>
+          {ANIMALS.map(animal => (
+            <option value={animal}>{animal}</option>
+          ))}
         </label>
         <button>Submit</button>
       </form>
