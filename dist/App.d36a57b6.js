74e8928f0ca36600a3f2cc9f6440069c1c32bc25
Sofia <sgandsteph@gmail.com>
2020-10-11 17:06:20 +0200

    runs mock directly env

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -30110,7 +30110,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63971" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56661" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
