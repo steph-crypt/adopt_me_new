4c8c3dce8b7f0e9df877153863f62776f7afc7db
Sofia <sgandsteph@gmail.com>
2020-10-20 14:19:27 +0200

    starts context file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { Router, Link  } from "@reach/router";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const themeHook = useState("darkblue");
   return (
     <div>
       <header>
