ab5f438f3f62ffd987ce982daa3db97a7dcde80b
Sofia <sgandsteph@gmail.com>
2020-10-24 17:36:12 +0200

    Creates markup and functions to initiate modal

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
+import { navigate } from "@reach/router";
+import Modal from "./Modal";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
 
 class Details extends React.Component {
-  state = { loading: true };
+  state = { loading: true, showModal: false };
   componentDidMount() {
     pet.animal(this.props.id).then(({ animal }) => {
         this.setState({
+          url: animal.url,
           name: animal.name,
           animal: animal.type,
           location: `${animal.contact.address.city}, ${
@@ -20,14 +23,17 @@ class Details extends React.Component {
           loading: false
         });
       })
-      .catch(err => this.setState({ error: err }));
+    .catch(err => this.setState({ error: err }));
   }
+
+toggleModal = () => this.setState({ showModal: !this.state.showModal });
+adopt = () => navigate(this.state.url);
   render() {
     if (this.state.loading) {
       return <h1>loading … </h1>;
     }
 
-    const { animal, breed, location, description, media, name } = this.state;
+    const { animal, breed, location, description, media, name, showModal  } = this.state;
 
     return (
       <div className="details">
@@ -37,9 +43,22 @@ class Details extends React.Component {
           <h2>{`${animal} — ${breed} — ${location}`}</h2>
           <ThemeContext.Consumer>
             {([theme]) => (
-               <button style={{ backgroundColor: theme }}>Adopt {name}</button> )}
+               <button onClick={this.toggleModal} style={{ backgroundColor: theme }}>Adopt {name}</button> )}
           </ThemeContext.Consumer>
           <p>{description}</p>
+          {
+            showModal ? (
+              <Modal>
+                <div>
+                  <h1>Would you like to adopt {name}?</h1>
+                  <div className="buttons">
+                    <button onClick={this.adopt}>Yes</button>
+                    <button onClick={this.toggleModal}>No</button>
+                  </div>
+                </div>
+              </Modal>
+            ) : null
+          }
         </div>
       </div>
     );
--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,25 +1,20 @@
 import React, { useEffect, useRef } from "react";
-import { createPortal } from " react-dom";
+import { createPortal } from "react-dom";
 
+const modalRoot = document.getElementById("modal");
 
 const Modal = ({ children }) => {
   const elRef = useRef(null);
-  if(!elRef.current) {
-    const div = document.createELement("div");
-    elref.current = div;
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
   }
-}
 
-useEffect(() => {
-  const modalRoot = document.getElementById("modal");
-  modalRoot.appendChild(elRef.current);
+  useEffect(() => {
+    modalRoot.appendChild(elRef.current);
+    return () => modalRoot.removeChild(elRef.current);
+  }, []);
 
-  return () => modalRoot.removeChild(elRef.current);
-}, []);
-
-return createPortal(<div>{children}</div>, elRef.current);
-
-}
+  return createPortal(<div>{children}</div>, elRef.current);
+};
 
 export default Modal;
-
